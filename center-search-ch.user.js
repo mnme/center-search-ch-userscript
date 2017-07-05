8993c12d8879631796f61084c231507e1baafd73
Nicolas Jeker <n.jeker@gmx.net>
2017-07-05 17:34:11 +0200

    Add https support, use GM_addStyle instead of jQuery

--- center-search-ch.user.js
+++ center-search-ch.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name Center search.ch website
-// @description 
+// @description
 // @author Nicolas Jeker
-// @version 1.0.0
-// @date 2015-04-04
-// @grant none
-// @include http://*.search.ch/*
+// @version 1.0.1
+// @date 2017-07-05
+// @grant GM_addStyle
+// @include /^https?://.*\.search\.ch/.*$/
 // @run-at document-end
 // @license MIT License
 // ==/UserScript==
 
-(function () {
-	$(".sl-cards-container,.sl-nav-container,.sl-footer").css("margin","0px auto");
-	$("#sl-tribune-big").hide();
-})();
+GM_addStyle([".sl-cards-container,.sl-nav-container,.sl-footer {",
+            "   margin: 0px auto; !important",
+            "}",
+            "#sl-tribune-big {",
+            "   display: none; !important",
+            "}"].join(""));
